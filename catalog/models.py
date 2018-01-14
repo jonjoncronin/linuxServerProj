395258086bf18810431b1826933c0c98aa049a71
Jon Cronin <jonjoncronin@gmail.com>
2018-01-14 00:31:26 -0800

    Migrate CatalogApp models and DB population script to support Postgresql

---
+++ catalog/models.py
@@ -0,0 +1,115 @@
+#!/usr/local/bin/python3
+"""
+The models.py module is a module intended define the object model for the
+Catalog Application.
+
+This object model should be used heavily by the application.py standalone
+module.
+"""
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import create_engine
+import random
+import string
+
+Base = declarative_base()
+
+
+class User(Base):
+    """
+    User class to represent users that wish to use the Catalog App.
+
+    Inheritence
+    =======================================================
+    Base -
+        A sqlalchemy declarative_base
+    """
+    __tablename__ = "AppUser"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+
+class Category(Base):
+    """
+    Category class to represent different categories set by users and for which
+    items fall under.
+
+    Inheritence
+    =======================================================
+    Base -
+        A sqlalchemy declarative_base
+    """
+    __tablename__ = "Category"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+
+    @property
+    def serialize(self):
+        """
+        A property of a Category that allows a caller to serialze the data that
+        represents a specific Category object
+
+        Parameters
+        =======================================================
+        self -
+            The category to serialize.
+
+        Returns
+        =======================================================
+        dictionary containing the information that makes up a Category.
+        """
+        return {
+            "id": self.id,
+            "name": self.name,
+        }
+
+
+class Item(Base):
+    """
+    Item class to represent different items set by users and for which fall
+    under different categories.
+
+    Inheritence
+    =======================================================
+    Base -
+        A sqlalchemy declarative_base
+    """
+    __tablename__ = "Item"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    description = Column(String)
+    category_id = Column(Integer, ForeignKey("Category.id"))
+    category = relationship(Category)
+    user_id = Column(Integer, ForeignKey("AppUser.id"))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """
+        A property of an Item that allows a caller to serialze the data that
+        represents a specific Item object
+
+        Parameters
+        =======================================================
+        self -
+            The item to serialize.
+
+        Returns
+        =======================================================
+        dictionary containing the information that makes up an Item.
+        """
+        return {
+            "cat_id": self.category_id,
+            "description": self.description,
+            "id": self.id,
+            "title": self.name,
+        }
+
+
+# engine = create_engine("sqlite:///catalog.db")
+engine = create_engine('postgresql://db_admin:admin@localhost:5432/catalogStore')
+
+Base.metadata.create_all(engine)
---
+++ catalog/populateDummyDb.py
@@ -0,0 +1,69 @@
+#!/usr/local/bin/python3
+"""
+The populateDummyDb.py module is a module intended utilize the object model for
+the Catalog Application and prepopulate the DB with some content to develop
+against.
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base, Item, Category, User
+
+# engine = create_engine("sqlite:///catalog.db")
+engine = create_engine('postgresql://db_admin:admin@localhost:5432/catalogStore')
+# Bind the engine to the metadata of the Base class so that the
+# declaratives can be accessed through a DBSession instance
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+# A DBSession() instance establishes all conversations with the database
+# and represents a "staging zone" for all the objects loaded into the
+# database session object. Any change made against the objects in the
+# session won't be persisted into the database until you call
+# session.commit(). If you're not happy about the changes, you can
+# revert all of them back to the last commit by calling
+# session.rollback()
+session = DBSession()
+
+itemsList = (
+    {"category": "kitchen",
+     "items": (
+         {"name": "fork", "description": ""},
+         {"name": "knife", "description": ""},
+         {"name": "spoon", "description": ""},
+         {"name": "plate", "description": ""},
+         {"name": "bowl", "description": ""})
+     },
+    {"category": "bathroom",
+        "items": (
+            {"name": "toothbrush", "description": ""},
+            {"name": "toothpaste", "description": ""},
+            {"name": "floss", "description": ""},
+            {"name": "razor", "description": ""},
+            {"name": "hairbrush", "description": ""})
+     }
+)
+
+# Create the admin user
+defUser = User(name="Admin", email="admin@example.com", picture="")
+session.add(defUser)
+session.commit()
+
+# Create dummy items
+for category in itemsList:
+    someCategory = Category(name=category["category"])
+    session.add(someCategory)
+    session.commit()
+    newCat = session.query(Category).filter_by(name=someCategory.name).one()
+    print("New Category {0}\n".format(category))
+    for item in category["items"]:
+        someItem = Item(
+            user_id=defUser.id,
+            name=item["name"],
+            description=item["description"],
+            category_id=newCat.id)
+
+        session.add(someItem)
+        print("added {0}\n".format(item))
+    print("=======================\n")
+session.commit()
+print("DB populated")
